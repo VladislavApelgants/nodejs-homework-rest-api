7e76c085d8623fbfaf25b6e5fcd6fbebc9d0cf0b
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-09-05 23:30:04 +0300

    Object.keys

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,7 +1,7 @@
 const { httpError } = require("../helpers");
 const validateBody = (shema) => {
   return function (req, res, next) {
-    if (!Object.entries(req.body).length) {
+    if (!Object.keys(req.body).length) {
       next(httpError(400, "missing fields"));
     }
     const { error } = shema.validate(req.body);
